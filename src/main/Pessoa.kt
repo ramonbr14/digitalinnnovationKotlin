abb31715a21eef5a58897cc6a02b4072e4dbaf07
ramonbr14 <ramonbr14@gmail.com>
2022-03-29 19:49:01 -0400

    Fundamentos de Orientação a Objetos com Kotlin Membros das classes na prática - Parte 2

--- src/main/Pessoa.kt
+++ src/main/Pessoa.kt
@@ -2,13 +2,17 @@ package main
 
 class Pessoa {
 
-    var nome: String = "Elsa";
+    var nome: String = "Elsa"
     var cpf: String = "123.456.789-00"
+    //private set
 }
 
 fun main(){
     var elsa = Pessoa()
+    elsa.cpf = "ABC.DEF.GHI-JK"
 
     println(elsa.nome)
     println(elsa.cpf)
 }
+
+
