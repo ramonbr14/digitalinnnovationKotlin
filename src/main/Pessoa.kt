da2868e247529b73cf4d64d97d4d5d7f5f02e406
ramonbr14 <ramonbr14@gmail.com>
2022-03-29 20:01:59 -0400

    Membros das classes na prática - Parte 3

--- src/main/Pessoa.kt
+++ src/main/Pessoa.kt
@@ -2,17 +2,20 @@ package main
 
 class Pessoa {
 
-    var nome: String = "Elsa"
+    var nome: String = "Rainha Elsa de Arendelle"
     var cpf: String = "123.456.789-00"
     //private set
+
+    constructor()
+
+    fun pessoainfo() = "$nome " + "$cpf"
 }
 
 fun main(){
     var elsa = Pessoa()
-    elsa.cpf = "ABC.DEF.GHI-JK"
+    //elsa.cpf = "ABC.DEF.GHI-JK"
 
-    println(elsa.nome)
-    println(elsa.cpf)
+    println(elsa.pessoainfo())
 }
 
 
